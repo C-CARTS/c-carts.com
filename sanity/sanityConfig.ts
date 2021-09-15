e5ba80f010818baa6fb3699834a8eb207fa2f868
Ryan Blackman <rblackman@mtd.org>
2021-09-15 08:44:41 -0500

    small cleanup

--- sanity/sanityConfig.ts
+++ sanity/sanityConfig.ts
@@ -1,13 +1,14 @@
-const projectId = process.env.NEXT_PUBLIC_SANITY_ID ?? 'No NEXT_PUBLIC_SANITY_ID';
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'No NEXT_PUBLIC_SANITY_DATASET';
+const projectId = process.env.NEXT_PUBLIC_SANITY_ID ?? 'No-NEXT-PUBLIC-SANITY-ID';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'No-NEXT-PUBLIC-SANITY-DATASET';
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? 'No-NEXT-PUBLIC-SANITY-API-VERSION';
 const previewMode = process.env.NEXT_PUBLIC_SANITY_PREVIEW === 'true';
 const useCdn = process.env.NEXT_PUBLIC_SANITY_USE_CDN === 'true';
-
 const config = {
 	projectId,
 	dataset,
 	previewMode,
 	useCdn,
+	apiVersion,
 	fetch
 };
 
