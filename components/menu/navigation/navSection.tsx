25fa2ebb01190e7d1d6e7aa8f8800b02ed862a36
Ryan Blackman <rblackman@mtd.org>
2021-09-15 08:51:58 -0500

    click outside menu closes

--- components/menu/navigation/navSection.tsx
+++ components/menu/navigation/navSection.tsx
@@ -3,6 +3,7 @@ import { FC, useEffect, useRef } from 'react';
 import { MdArrowDropDown } from 'react-icons/md';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { MainNavItem } from '../../../data-hooks/useMainNav';
+import useOnClickOutside from '../../../hooks/useOnClickOutside';
 import styles from '../../../styles/nav-section.module.scss';
 import { focusIndexState, isOpenSelector, openIndexState } from './state';
 import SubPage from './subPage';
@@ -16,9 +17,17 @@ const NavSection: FC<Props> = ({ item: { title, subPages, slug }, index }: Props
 	const buttonRef = useRef<HTMLButtonElement | null>(null);
 	const setOpenIndex = useSetRecoilState(openIndexState);
 	const [focusIndex, setFocusIndex] = useRecoilState(focusIndexState);
-
+	const menuRef = useRef<HTMLDivElement | null>(null);
 	const isOpen = useRecoilValue(isOpenSelector(index));
 
+	// close menu on click outside
+	useOnClickOutside(menuRef, () => {
+		if (isOpen) {
+			setOpenIndex(null);
+		}
+	});
+
+	// set focus if the focus index is set
 	useEffect(() => {
 		if (focusIndex === index && buttonRef.current) {
 			buttonRef.current.focus();
@@ -35,7 +44,7 @@ const NavSection: FC<Props> = ({ item: { title, subPages, slug }, index }: Props
 	}
 
 	return (
-		<div className={styles.sectionWrap}>
+		<div className={styles.sectionWrap} ref={menuRef}>
 			<button type="button" onClick={buttonClick} ref={buttonRef} className={`${styles.navButton} ${isOpen ? styles.open : styles.closed}`}>
 				<MdArrowDropDown />
 				{title}
