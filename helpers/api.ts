af7d4ce3092af2e48233e857bd5a6a584b658234
Ryan Blackman <rblackman@mtd.org>
2024-05-09 16:07:44 -0500

    feature: add revalidate

--- helpers/api.ts
+++ helpers/api.ts
@@ -7,6 +7,9 @@ import type SiteConfig from "../@types/siteConfig";
 import { client } from "../sanity/lib/client";
 import throwError from "./throwError";
 
+const isDev = process.env.NODE_ENV === "development";
+const cache = isDev ? "no-store" : "force-cache";
+
 const settingsName =
 	process.env.NEXT_PUBLIC_SETTINGS_DOC_NAME ??
 	throwError("No NEXT_PUBLIC_SETTINGS_DOC_NAME");
@@ -17,7 +20,14 @@ const homepageName =
 
 export async function getSiteConfig(): Promise<SiteConfig> {
 	const query = `*[ _id == '${settingsName}' && _type == 'siteConfig' ][0]`;
-	return await client.fetch(query);
+	return await client.fetch(
+		query,
+		{},
+		{
+			cache,
+			next: { tags: [settingsName] },
+		},
+	);
 }
 
 export async function getNavItems(): Promise<NavItem[]> {
@@ -27,23 +37,51 @@ export async function getNavItems(): Promise<NavItem[]> {
 		slug,
 		children[]->{ _id, title, slug }
 	  }`;
-	return await client.fetch<NavItem[]>(query);
+	return await client.fetch<NavItem[]>(
+		query,
+		{},
+		{
+			cache,
+			next: { tags: [settingsName] },
+		},
+	);
 }
 
 export async function getHomepage(): Promise<Page> {
 	const query = `*[ _id == '${homepageName}' && _type == 'page' ][0]`;
-	return await client.fetch(query);
+	return await client.fetch(
+		query,
+		{},
+		{
+			cache,
+			next: { tags: [settingsName] },
+		},
+	);
 }
 
 export async function getPage(slug: string): Promise<Page> {
 	const query = `*[ slug.current == '${slug}' && _type == 'page' ][0]`;
-	const data = await client.fetch(query);
+	const data = await client.fetch(
+		query,
+		{},
+		{
+			cache,
+			next: { tags: [settingsName] },
+		},
+	);
 	return data;
 }
 
 export async function getImage(id: string): Promise<ImageData> {
 	const query = `*[ _id == '${id}' && _type == 'sanity.imageAsset' ][0]`;
-	const data = await client.fetch(query);
+	const data = await client.fetch(
+		query,
+		{},
+		{
+			cache,
+			next: { tags: ["images"] },
+		},
+	);
 	return data;
 }
 
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@sanity/client";
+import "server-only";
+import { apiVersion, dataset, projectId } from "../env";
 
-import { apiVersion, dataset, projectId, useCdn } from "../env";
+const isDev = process.env.NODE_ENV === "development";
 
 export const client = createClient({
 	apiVersion,
 	dataset,
 	projectId,
-	useCdn,
+	useCdn: isDev ? true : false,
 });
