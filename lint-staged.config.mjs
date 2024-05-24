e3dce1c49881ab432991daa88712ffec46568806
Ryan Blackman <rblackman@mtd.org>
2024-05-24 16:44:14 -0500

    update lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,14 +1,18 @@
 import path from "path";
 
-const buildEslintCommand = (filenames) => [
-	`next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`,
-];
+const buildEslintCommand = (filenames) => {
+	const relativePaths = filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(" --file ");
+	return `next lint --fix --file ${relativePaths}`;
+};
 
 const config = {
-	"**/*.(ts|tsx)": () => "npx tsc --noEmit",
-	"*.{js,jsx,ts,tsx,mjs}": [buildEslintCommand],
-	"**/*.(md|json)": (filenames) =>
+	"**/*.{ts,tsx}": () => "npx tsc --noEmit",
+	"**/*.{js,jsx,ts,tsx,mjs}": (filenames) => [buildEslintCommand(filenames)],
+	"**/*.{md,json}": (filenames) =>
 		`npx prettier --write ${filenames.join(" ")}`,
+	"package-lock.json": () => "echo 'Ignoring package-lock.json'",
 };
 
 export default config;
